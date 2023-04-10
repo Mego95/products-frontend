c23e0d4bc9f38a161a542aa52a2da0da3113f76b
hmego <h.mego9@gmail.com>
2023-04-10 21:08:18 +0300

    v0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './app.interfaces';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'products-frontend';
+
+  usersMenu: MenuItem[] = [
+    {text: 'List all users', link: 'user/list'},
+    {text: 'Insert a user', link: 'not-implemented-yet'},
+    {text: 'Delete a user', link: 'not-implemented-yet'},
+    {text: 'Update a user', link: 'not-implemented-yet'},
+  ];
+  productsMenu: MenuItem[] = [
+    {text: 'List all products', link: 'not-implemented-yet'},
+    {text: 'Insert a product', link: 'not-implemented-yet'},
+    {text: 'Delete a product', link: 'not-implemented-yet'},
+    {text: 'Update a product', link: 'not-implemented-yet'},
+  ];
 }
