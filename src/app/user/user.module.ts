b8993e3ade89e6a5a02b955aeaa5f5c71166819d
hmego <h.mego9@gmail.com>
2023-04-25 20:52:53 +0300

    canActivate userGuard

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,19 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { UserService } from './user.service';
+import { AppService } from '../app.service';
 
 import { UsersListComponent } from './users-list/users-list.component';
 import { UserInsertComponent } from './user-insert/user-insert.component';
 import { WelcomeComponent } from './welcome/welcome.component'
+import { tap } from 'rxjs';
+
+const userGuard = () => {
+  const router = inject(Router);
+  const appService = inject(AppService);
+
+  return appService.isLoggedIn$.pipe(
+    tap(isLoggedIn => {
+      if (!isLoggedIn) router.navigate(['/login']);
+      // return !isLoggedIn? router.navigate(['/login']) : true;
+    })
+  )
+}
 
 const routes: Routes = [
-  {path: 'list', component: UsersListComponent},
-  {path: 'insert', component: UserInsertComponent},
-  {path: '', component: WelcomeComponent},
+  {path: 'list', component: UsersListComponent, canActivate: [userGuard]},
+  {path: 'insert', component: UserInsertComponent, canActivate: [userGuard]},
+  {path: '', component: WelcomeComponent, canActivate: [userGuard]},
 ];
 
 @NgModule({
